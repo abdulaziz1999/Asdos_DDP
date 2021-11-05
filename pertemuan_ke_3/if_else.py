faa203a3f0903cba9b7d52eb791d5ecc7fb5e620
abdulaziz1999 <azizmentor96@gmail.com>
2021-11-05 10:02:44 +0700

    if else example

--- pertemuan_ke_3/if_else.py
+++ pertemuan_ke_3/if_else.py
@@ -1,8 +1,16 @@
 nama = input("Masukan Nama Anda : ")
 umur = int(input("Masukan umur anda : "))
+hobi = input("Masukan Hobi :")
 if(umur >= 25):
-    print(nama,"Kamu Tua")
+    print(nama,"Kamu Berumur Tua")
 elif(umur <= 17):
     print(nama,"Kamu Bocil")
 else:
     print(nama,"Kamu Remaja")
+    
+if(hobi == 'renang' or hobi == 'Renang'):
+    print("Selamat Hobi kamu",hobi)
+elif(hobi == 'takraw'):
+    print("Selamat Hobi kamu",hobi)
+else:
+    print("hobi kamu selain renang dan takraw")
---
+++ pertemuan_ke_3/latihan_if_else.py
@@ -0,0 +1,6 @@
+saldo_tiket = 0
+
+if(saldo_tiket > 0): #kondisi atau keadaan
+    print("Boleh Masuk")#True
+else:
+    print("Silahkan Isi Saldo Anda")#False
