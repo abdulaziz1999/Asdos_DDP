26951d1ff486dcbceb403666e5182332859e2073
abdulaziz1999 <azizmentor96@gmail.com>
2021-11-05 10:03:21 +0700

    for  item

--- pertemuan_ke_4/for_item.py
+++ pertemuan_ke_4/for_item.py
@@ -1,4 +1,8 @@
 item = ['vario','beat','ninja','revo','mio']
+item2 = ['kucing','tikus','marmut','kura-kura']
 
 for merek in item:
     print(merek)
+    
+for hewan in item2:
+    print('Nama Hewan :',hewan)
